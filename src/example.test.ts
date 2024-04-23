f8ec7ea7c51e19308632cf0cc8d24fba8ceaee9f
Sn-Kinos <dony435@naver.com>
2024-04-23 18:03:11 +0900

    initial commit

---
+++ src/example.test.ts
@@ -0,0 +1,46 @@
+import { Entity, MikroORM, PrimaryKey, Property } from '@mikro-orm/mariadb';
+
+@Entity()
+class User {
+  @PrimaryKey()
+  id!: number;
+
+  @Property()
+  name: string;
+
+  @Property({ type: 'json', unique: true })
+  email: object;
+
+  constructor(name: string, email: object) {
+    this.name = name;
+    this.email = email;
+  }
+}
+
+let orm: MikroORM;
+
+beforeAll(async () => {
+  orm = await MikroORM.init({
+    entities: [User],
+    debug: ['query', 'query-params'],
+    allowGlobalContext: true, // only for testing
+    forceEntityConstructor: true,
+    clientUrl: 'your-mariadb-11.3-url',
+  });
+  await orm.schema.refreshDatabase();
+});
+
+afterAll(async () => {
+  await orm.close(true);
+});
+
+test('basic CRUD example', async () => {
+  orm.em.create(User, { name: 'Foo', email: { a: 1 } });
+  await orm.em.flush();
+  orm.em.clear();
+
+  await orm.em.transactional(async (em) => {
+    const user = await orm.em.findOneOrFail(User, { name: 'Foo' });
+    expect(user.name).toBe('Foo');
+  });
+});
